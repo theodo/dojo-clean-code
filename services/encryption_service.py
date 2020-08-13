dffe56fcb7132af6775a414f20043f180baa7f89
Francois Hendriks <Francois.Hendriks-ext@dashlane.com>
2020-08-13 19:00:52 +0200

    chore: remove unused packages

--- services/encryption_service.py
+++ services/encryption_service.py
@@ -4,8 +4,6 @@ import json
 from base64 import b64encode, b64decode
 import hashlib
 from Cryptodome.Cipher import AES
-import os
-from Cryptodome.Random import get_random_bytes
 
 
 def encrypt(plain_text, password):
