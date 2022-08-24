91cf6cd703ccb20c3320ccfff63a209b1af4a92e
Francois Hendriks <francoishendriks96@gmail.com>
2022-08-25 00:20:54 +0200

    chore: format password distance

--- services/password_distance.py
+++ services/password_distance.py
@@ -4,7 +4,7 @@ def compute_levenshtein_distance(string1, string2):
 
 
 if __name__ == "__main__":
-    assert(compute_levenshtein_distance("Python", "Peithen") == 3)
-    assert(compute_levenshtein_distance("Azerty12", "Azerty14") == 1)
-    assert(compute_levenshtein_distance("Toto", "toto13") == 3)
-    assert(compute_levenshtein_distance("JohnDoe2001", "Johndoe2002") == 2)
+    assert (compute_levenshtein_distance("Python", "Peithen") == 3)
+    assert (compute_levenshtein_distance("Azerty12", "Azerty14") == 1)
+    assert (compute_levenshtein_distance("Toto", "toto13") == 3)
+    assert (compute_levenshtein_distance("JohnDoe2001", "Johndoe2002") == 2)
